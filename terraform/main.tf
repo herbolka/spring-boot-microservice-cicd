a8a2b423015f9e4962a9cbb20e540391e998ae23
Ihor Herbolka <ihor_herbolka@epam.com>
2026-04-08 23:46:35 +0300

    Update cloudwatch settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -356,7 +356,6 @@ resource "aws_ecr_lifecycle_policy" "microservice" {
 resource "aws_cloudwatch_log_group" "eks" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = 1
-  skip_destroy      = true
 
   lifecycle {
     # Terraform will refuse to destroy this resource
