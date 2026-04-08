6e8420ad88e91e3be4e547c6b906bda8e5901ae8
Ihor Herbolka <ihor_herbolka@epam.com>
2026-04-08 19:59:36 +0300

    Upgrade kubernetes version to 1.31

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -19,7 +19,7 @@ variable "cluster_name" {
 variable "kubernetes_version" {
   description = "Kubernetes version to use for the EKS cluster"
   type        = string
-  default     = "1.28"
+  default     = "1.31"
 }
 
 # VPC Configuration
